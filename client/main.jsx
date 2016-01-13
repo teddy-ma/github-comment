1136dbd2a938cd472da1f9eeca1085e50fc2fc6b
马陆骋 <malucheng@boohee.com>
2016-01-14 00:08:43 +0800

    feat: fetch comments success

--- client/main.jsx
+++ client/main.jsx
@@ -9,7 +9,13 @@ var comment_url = "http://localhost:5000/fake/comments";
 var comments_url = "http://localhost:5000/fake/comments";
 var loading_img = "http://localhost:5000/images/boohee.gif";
 
+var default_avatar_url = "http://github-comment.herokuapp.com/images/boohee.png";
+
+comments_url = "http://github-comment.herokuapp.com/comments?page_id=4&user_name=teddy-ma&repo=teddy-ma.github.io";
+auth_url = "http://github-comment.herokuapp.com/users/auth";
+
 // 评论表单容器组件
+//{"auth":false,"login_url":"https://github.com/login/oauth/authorize?scope=public_repo&client_id=c518ebe97832f22e306a"}
 var FormBox = React.createClass({
   getInitialState: function() {
     var ret = null;
@@ -24,10 +30,10 @@ var FormBox = React.createClass({
       if(data.auth){
         ret = {name: data.user_name, avatar: data.avatar_url, auth: true};
       }else{
-        ret = {name: "请登录", avatar: "http://github-comment.herokuapp.com/images/boohee.png", auth: false};
+        ret = {name: "请登录", avatar: default_avatar_url, auth: false, login_url: data.login_url};
       }
     }).fail(function(xhr)  {
-       ret = {name: "请登录", avatar: "http://github-comment.herokuapp.com/images/boohee.png", auth: false};
+       ret = {name: "请登录", avatar: default_avatar_url, auth: false};
     });
     return ret;
   },
@@ -35,7 +41,7 @@ var FormBox = React.createClass({
     return (
       <div className={style.github_comment_form_wrapper}>
         <Avatar name={this.state.name} avatar={this.state.avatar} />
-        <Form auth={this.state.auth}/>
+        <Form auth={this.state.auth} login_url={this.state.login_url}/>
       </div>
     );
   }
@@ -74,7 +80,7 @@ var Form = React.createClass({
               <button disabled={this.state.submited} onClick={this.handleClick} type="button">提交</button>
             </div>
             :
-            <button>Login via GitHub</button>
+            <a className="button" target="_blank" href={this.props.login_url}>Login via GitHub</a>
         }
       </div>
     );
@@ -116,7 +122,7 @@ var List = React.createClass({
       <div className={style.github_comment_items_wrapper}>
         {
           this.state.loading ? <img src={loading_img} /> : this.state.comments.map(function(item){
-            return <Comment key={item.id} content={item.body} avatar={item.avatar_url} name={item.user_name}/>
+            return <Comment key={item.id} content={item.body} avatar={item.user.avatar_url} name={item.user.login}/>
           })
         }
       </div>
