c77801b8cecee5d137b5c98bc6060eba8ebe4f77
teddy <mlc880926@gmail.com>
2018-05-15 22:19:57 +0800

    fix create comment

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -4,28 +4,60 @@ var router = express.Router();
 
 // 发起评论
 router.post('/', function(req, res, next) {
-  var body = req.body.body;
+  var content = req.body.body;
   var repo = req.body.repo;
   var page_id = req.body.page_id;
   var user_name = req.body.user_name;
-  var url = 'https://api.github.com/repos/' + user_name + '/' + repo + '/issues/' + page_id + '/comments';
-  console.log(req.session.token);
-  request.post({
+
+  // get issue number first
+  const isssues_url = `https://api.github.com/repos/${user_name}/${repo}/issues`;
+  request({
+    url: isssues_url,
     headers: {
-      "Authorization": "token " + req.session.token,
+      "Authorization": "token " + PERSON_TOKEN,
       'user-agent': 'node.js',
       'Content-Type': 'application/json'
-    },
-    url: url,
-    json: {
-      body: body
     }
-  }, function(error, response, body) {
-    console.log(error);
-    console.log(body);
-    res.json({
-      body
-    });
+  }, (error, response, body) => {
+    // debugger;
+    const issue = JSON.parse(body).filter(c=> c.title === page_id)[0];
+    
+    if(issue) { // issue exist
+      var issue_id = issue.number;     
+      var url = 'https://api.github.com/repos/' + user_name + '/' + repo + '/issues/' + issue_id + '/comments';      
+      request.post({
+        headers: {
+          "Authorization": "token " + req.session.token,
+          'user-agent': 'node.js',
+          'Content-Type': 'application/json'
+        },
+        url: url,
+        json: {
+          body: content
+        }
+      }, function(error, response, body) {  
+        res.json({
+          body
+        });
+      });
+    } else {
+      create_issue(isssues_url, PERSON_TOKEN, page_id, (error, result) => {
+        if(!error) {
+          var issue_id = result.number;
+          var comments_url = "https://api.github.com/repos/" + user_name + "/" + repo + "/issues/" + issue_id + "/comments";
+          request({
+            url: comments_url,
+            headers: {
+              "Authorization": "token " + PERSON_TOKEN,
+              'user-agent': 'node.js',
+              'Content-Type': 'application/json'
+            }
+          }, (error, response, body) => {
+            res.json(JSON.parse(body)); 
+          });
+        }
+      });
+    };
   });
 });
 
