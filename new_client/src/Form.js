cd441e8aca5fe5b30bae9dc1eec13969ab2cfe0d
teddy <mlc880926@gmail.com>
2019-02-07 17:13:56 +0800

    add form

---
+++ new_client/src/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" /> comment
+      </div>
+    )
+  }
+}
+
+export default Form;
