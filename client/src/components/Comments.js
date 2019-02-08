0cda1b0c7ac351549b72fba8d4723a0e792d818c
teddy <mlc880926@gmail.com>
2019-02-08 10:01:19 +0800

    add content loader

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 import Item from './Item.js';
+import ContentLoader, { Facebook } from "react-content-loader";
+
 
 class Comments extends Component {
 
   render() {
     if (this.props.loading) {
       return (
-        <p>loading ....</p>
+        <ContentLoader>
+          <rect x="20" y="20" rx="5" ry="5" width="40" height="40" />
+          <rect x="80" y="17" rx="4" ry="4" width="300" height="7" />
+          <rect x="80" y="40" rx="3" ry="3" width="250" height="7" />
+        </ContentLoader>
       );
     } else {
       return (
